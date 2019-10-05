0cea935890393e3ea10e62d28001fa191fb9683a
jules blanc <jules.blanc@unccub.com>
2019-10-05 09:46:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,7 @@ def hit?(number)
   if get_user_input == 'h'
     deal_card
     card_total += deal_card
-  else if get_user_input == 's'
+  elsif get_user_input == 's'
     deal_card
   else 
     invalid_command
